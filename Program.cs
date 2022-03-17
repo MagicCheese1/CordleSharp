e29d58d54ad34b464bf79c06a67131625ff952db
Kacper Płażewski <kacpik@mapik.eu>
2022-03-17 17:33:54 +0100

    Load words

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
 namespace CordleSharp {
     public class Program {
+        private static string allowedLetters = "QWERTYUIOPASDFGHJKLZXCVBNM";
         public static void Main (string[] args) {
+            //PossibleWords is used for generating the winning word.
+            //Both PossibleWords and AllowedWords are used for checking if the players input is valid.
+            var possibleWordsFile = File.ReadAllLines ("L:/CordleSharp/PossibleWords.txt");
+            var allowedWordsFile = File.ReadAllLines ("L:/CordleSharp/AllowedWords.txt");
+            List<String> PossibleWords = new List<string> (possibleWordsFile);
+            List<String> AllowedWords = new List<string> (allowedWordsFile);
         }
     }
 }
