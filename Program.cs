43d9b9072ae09bdb088425b4d3c057c4d061b702
Kacper Płażewski <kacpik@mapik.eu>
2022-03-17 17:34:53 +0100

    Generate the winning word

--- Program.cs
+++ Program.cs
@@ -8,6 +8,10 @@ namespace CordleSharp {
             var allowedWordsFile = File.ReadAllLines ("L:/CordleSharp/AllowedWords.txt");
             List<String> PossibleWords = new List<string> (possibleWordsFile);
             List<String> AllowedWords = new List<string> (allowedWordsFile);
+            Random random = new Random ();
+
+            // string TheWinningWord = "eblbe";
+            string TheWinningWord = PossibleWords[random.Next (1, PossibleWords.Count () - 1)];
         }
     }
 }
